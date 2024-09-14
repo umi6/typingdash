03a65a9ddcafc7a665701790a31dc627f25db2c9
umi6 <124774831+umi6@users.noreply.github.com>
2024-09-14 14:13:48 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+<div style="position: relative; width: 100%; padding-top: 56.25%;">
+    <iframe src="https://typingdash.netlify.app/" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;"></iframe>
+</div>
